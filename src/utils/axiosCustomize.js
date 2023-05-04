5d7778f7678cad8ad5d1c708c5433235edfc707f
Duy Nguyen <nguynnguynduy@gmail.com>
2023-05-04 23:15:49 +0700

    a

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -19,6 +19,7 @@ instance.interceptors.response.use(function (response) {
     // Do something with response data
     return response && response.data ? response.data : response;
 }, function (error) {
+    console.log(1)
     window.location.href = "/err"
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
